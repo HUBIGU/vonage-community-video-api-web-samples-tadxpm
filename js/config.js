bbf091cd58c1b8342d135d26704ae1f00649b4bd
HUBIGU <git@stackblitz.com>
2024-11-06 11:20:27 +0900

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+const SAMPLE_SERVER_BASE_URL =
+  'https://neru-48221537-vonage-video-backend-server-dev.apse1.runtime.vonage.cloud';
+
+// OR, if you have not set up a web server that runs the sample-video-php-learning_server code,
+// set these values to Vonage Application ID, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tools.vonage.com/video/playground.
+
+const APPLICATION_ID = '95fa26b0-e6af-4d32-b6d7-43b316d2df98';
+const SESSION_ID =
+  '2_MX45NWZhMjZiMC1lNmFmLTRkMzItYjZkNy00M2IzMTZkMmRmOTh-fjE3MzA0NDY3NTkxNzh-cVhySVZwaXQxbUpzNjFBcmdpa1Z3Q245fn5-';
+const TOKEN =
+  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNlc3Npb24uY29ubmVjdCIsInNlc3Npb25faWQiOiIyX01YNDVOV1poTWpaaU1DMWxObUZtTFRSa016SXRZalprTnkwME0ySXpNVFprTW1SbU9UaC1makUzTXpBME5EWTNOVGt4TnpoLWNWaHlTVlp3YVhReGJVcHpOakZCY21kcGExWjNRMjQ1Zm41LSIsInJvbGUiOiJtb2RlcmF0b3IiLCJpbml0aWFsX2xheW91dF9jbGFzc19saXN0IjoiIiwiZXhwIjoxNzMwNTMzMTU5LCJzdWIiOiJ2aWRlbyIsImFjbCI6eyJwYXRocyI6eyIvc2Vzc2lvbi8qKiI6e319fSwianRpIjoiY2M3MTliOGMtYTFiNy00ZjkyLWI0MTQtNTkwMzI3NGFkOTMxIiwiaWF0IjoxNzMwNDQ2NzU5LCJhcHBsaWNhdGlvbl9pZCI6Ijk1ZmEyNmIwLWU2YWYtNGQzMi1iNmQ3LTQzYjMxNmQyZGY5OCJ9.nCT4RlMym8XjhLpAZRzHOFdm4khaizCnaYj_Ust2YZL0XMB7_1snRkpnQOYXPMliuq1XKPfKB0pweL5tLtpmGVmX7KxrbTJac2JuAkFu__8qjWGltdUZNhP5KfnbDcRQaRuJ6g_vPtR6WGccxchCNNFv8-zaUQrwPuyR3lLJwm_YlLrPD1DbzIwrberM9fw59IJfc52gjqtg30RaQNdOtnH5yoJPx9pBG0de17FdIktQwOg-186vhkH5en8ts5r8OrLy40xsX31THcuNlb1MDR7JMFZ9MIXhgc8dtnqZtuZGPXXKTqR_g-05uEbwpvq3-T1HC2tj4qKL2fSR_63cwg';
